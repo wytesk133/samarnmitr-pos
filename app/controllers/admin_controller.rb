76f599944c4979ca619742adc2eed1e0bbc62695
Waitaya Krongapiradee <wyte.sk133@gmail.com>
2015-10-01 18:17:53 -0700

    Add stock details

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -7,8 +7,15 @@ class AdminController < ApplicationController
 
   def dashboard
     @total = 0
+    @not_shipped = Hash.new(0)
     Order.where(paid: true).each do |order|
       @total += order.total
+      bought = order.bought
+      received = order.received
+      bought.each do |key, value|
+        @not_shipped[key] += value-received[key]
+      end
     end
+    @products = Product.all
   end
 end
