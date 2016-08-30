7f728d83c3620562b33cd2899821a0e4d19cf07c
Waitaya Krongapiradee <wyte.sk133@gmail.com>
2016-08-30 16:48:33 +0700

    Add payment timestamp

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,11 @@
 class Product < ActiveRecord::Base
   validates_presence_of :name, :price
-  scope :available, -> { where(hidden: false) }
+  scope :available, -> { where.not(hidden: true) }
   has_many :stocks
 
   def current_stock
     sum = 0
-    self.stocks.each do |stock|
+    self.stocks.find_each do |stock|
       sum += stock.delta
     end
     sum
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150903054418) do
+ActiveRecord::Schema.define(version: 20160830082539) do
 
   create_table "combos", force: :cascade do |t|
     t.string   "name",        limit: 255
@@ -29,9 +29,9 @@ ActiveRecord::Schema.define(version: 20150903054418) do
     t.integer  "user_id",    limit: 4
     t.text     "customer",   limit: 65535
     t.integer  "total",      limit: 4
-    t.boolean  "paid",                     null: false
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.datetime "paid_at"
   end
 
   add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
